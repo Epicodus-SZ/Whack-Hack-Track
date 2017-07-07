f26ca8a8dd4c0c9b8accbc26d21c8411034f3505
Steve Zaske <github@zaske.com>
2017-07-07 14:25:04 -0700

    Ability to add teams

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -11,14 +11,17 @@ public class App {
     String layout = "templates/layout.vtl";
 
     //Add dummy default data
-    Team testTeam1 = new Team("Flogging Mollys");
-    Team testTeam2 = new Team("Five Bunnys");
-    Team testTeam3 = new Team("The Snacks Overflow");
-    Member default1 = new Member("steve@zaske.com");
-    Member default2 = new Member("billg@microsoft.com");
+    Team testTeam1 = new Team("The Justice League");
+    Team testTeam2 = new Team("The Avengers");
+    Team testTeam3 = new Team("Suicide Squad");
+    Member default1 = new Member("batman@dc.com");
+    Member default2 = new Member("CaptAmerica@marvel.com");
     Member default3 = new Member("hulk@marvel.com");
-    testTeam2.addMember(default1);
+    Member default4 = new Member("superman@dc.com");
+    testTeam1.addMember(default1);
+    testTeam1.addMember(default4);
     testTeam2.addMember(default2);
+    testTeam2.addMember(default3);
 
     //list all members
     get("/members", (request, response) -> {
@@ -44,10 +47,16 @@ public class App {
     }, new VelocityTemplateEngine());
 
     //list all teams
-    get("teams", (request, response) -> {
+    get("/teams", (request, response) -> {
       Map<String, Object> model = new HashMap<String, Object>();
       model.put("teams", Team.getAll());
       model.put("template", "templates/teams.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    get("/teams/new", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      model.put("template", "templates/teams-new.vtl");
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
 
@@ -78,6 +87,15 @@ public class App {
         team.addMember(member);
       }
       model.put("template", "templates/team.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    post("/teams/new", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      String teamName = request.queryParams("teamName");
+      Team team = new Team(teamName);
+      model.put("teams", Team.getAll());
+      model.put("template", "templates/teams.vtl");
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
 
